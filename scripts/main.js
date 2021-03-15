23acaef6f16985fe104fde6c11b911627733f8ff
Daan <daanselier2001@hotmail.nl>
2021-03-15 19:04:16 +0100

    Eerste commit tweede challenge
    Alle basiscode toegevoegd aan de html. Ik heb meegedaan in de les om de javascript code voor de klok draaiende te krijgen. Het is mij gelukt deze code over te kunnen zetten naar de html voor de nieuwe challenge.

---
+++ scripts/main.js
@@ -0,0 +1,45 @@
+function klok() {
+    var today = new Date();
+    var seconds = today.getSeconds();
+    var minutes = today.getMinutes();
+    var hours = today.getHours();
+
+    //if (hours >= 9 && hours < 21) {
+    //    document.getElementById('id').classList.add 
+    //}
+
+    if (seconds < 10) {
+        seconds = '0' + seconds;
+    }
+
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+
+    document.getElementById('clock').innerHTML = today.getHours() + ':' + minutes + ':' + seconds;
+}
+
+klok();
+setInterval(klok, 1000);
+
+// document.getElementById('clock').innerHTML = today.toString(); 
+
+//document.getElementById('clock').innerHTML = today.getDate();
+//document.getElementById('clock').innerHTML = today.getMonth(); // Januari is maand 0
+//document.getElementById('clock').innerHTML = today.getYear(); // Deprecated - niet gebruiken.
+//document.getElementById('clock').innerHTML = today.getFullYear();
+//document.getElementById('clock').innerHTML = today.getDay(); // Zondag is dag 0
+
+//document.getElementById('clock').innerHTML = today.getHours();
+//document.getElementById('clock').innerHTML = today.getMinutes();
+//document.getElementById('clock').innerHTML = today.getSeconds();
+//document.getElementById('clock').innerHTML = today.getMilliseconds();
+
+//document.getElementById('clock').innerHTML = today.getDate() + '/' + (today.getMonth()+1);
+
+//var maanden = new Array('januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december');
+//document.getElementById('clock').innerHTML = today.getDate() + ' ' + maanden[today.getMonth()];
+
+
+//document.getElementById('clock').innerHTML = today.getTime();
+
