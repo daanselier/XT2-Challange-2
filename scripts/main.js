5ce16bd984dedb6e482a51929448227a47bb33b6
Daan <daanselier2001@hotmail.nl>
2021-03-16 14:53:35 +0100

    Middagsessie 16 Maart
    Datum toegevoegd, op basis van de werkcollege oefeningen, om de astronauten een extra gevoel van tijd de geven 🚀. De datum heeft dezelfde classes toegevoegd gekregen als de klok binnen zijn eigen functie.
    
    We maken grote stappen voor de officiële launch op vrijdag 19 Maart.

--- scripts/main.js
+++ scripts/main.js
@@ -25,27 +25,26 @@ function klok() {
     document.getElementById('clock').innerHTML = today.getHours() + ':' + minutes + ':' + seconds;
 }
 
+function datum() {
+    var today = new Date();
+    var seconds = today.getSeconds();
+    var minutes = today.getMinutes();
+    var hours = today.getHours();
+    var maanden = new Array('januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december');
+
+    if (hours >= 9 && hours < 21) {
+        document.getElementById('date').classList.remove('night');
+        document.getElementById('date').classList.add('day');
+    } else {
+        document.getElementById('date').classList.remove('day');
+        document.getElementById('date').classList.add('night');
+    }
+
+    document.getElementById('date').innerHTML = today.getDate() + ' ' + maanden[today.getMonth()];
+}
+
 klok();
 setInterval(klok, 1000);
 
-// document.getElementById('clock').innerHTML = today.toString(); 
-
-//document.getElementById('clock').innerHTML = today.getDate();
-//document.getElementById('clock').innerHTML = today.getMonth(); // Januari is maand 0
-//document.getElementById('clock').innerHTML = today.getYear(); // Deprecated - niet gebruiken.
-//document.getElementById('clock').innerHTML = today.getFullYear();
-//document.getElementById('clock').innerHTML = today.getDay(); // Zondag is dag 0
-
-//document.getElementById('clock').innerHTML = today.getHours();
-//document.getElementById('clock').innerHTML = today.getMinutes();
-//document.getElementById('clock').innerHTML = today.getSeconds();
-//document.getElementById('clock').innerHTML = today.getMilliseconds();
-
-//document.getElementById('clock').innerHTML = today.getDate() + '/' + (today.getMonth()+1);
-
-//var maanden = new Array('januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december');
-//document.getElementById('clock').innerHTML = today.getDate() + ' ' + maanden[today.getMonth()];
-
-
-//document.getElementById('clock').innerHTML = today.getTime();
-
+datum();
+setInterval(datum, 100000);
